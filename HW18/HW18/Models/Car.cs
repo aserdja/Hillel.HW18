6c35d6094e9e9e6121db27f3ef6c0f18e87a1d90
Andrii Serdiuk <andreyserdjuk1@gmail.com>
2024-11-08 12:53:22 +0100

    Created Car model

---
+++ HW18/HW18/Models/Car.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HW18.Models
+{
+	public class Car
+	{
+		public int Id { get; set; }
+
+		[Required(ErrorMessage = "Brand must be entered!")]
+		[StringLength(30, MinimumLength = 1)]
+		public string Brand { get; set; } = string.Empty;
+
+		[Required(ErrorMessage = "Model must be entered!")]
+		public string Model { get; set; } = string.Empty;
+
+		[Required(ErrorMessage = "Price must be entered!")]
+		public decimal Price { get; set; }
+	}
+}
