9a222be8f00d2c16a06f65eee4c84b942cac7ddf
Andrii Serdiuk <andreyserdjuk1@gmail.com>
2024-11-08 12:56:02 +0100

    Created DbContext class. Made CodeFirst

---
+++ HW18/HW18/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using HW18.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW18.Data
+{
+    public class AppDbContext : DbContext
+    {
+		public virtual DbSet<Car> Cars => Set<Car>();
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer();
+		}
+	}
+}
